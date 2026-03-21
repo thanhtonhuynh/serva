50d6356fb2a15fe72c6421717798cbf4f93a9095
Ton Huynh <kintonhtt@gmail.com>
2026-03-20 22:16:57 -0700

    new not-found page ui

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,21 +1,35 @@
-import { CircleAlert } from "lucide-react";
+import { Container } from "@/components/layout";
+import { SIcon, Typography } from "@/components/shared";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import Link from "next/link";
 
 export default async function NotFound() {
   return (
     <>
-      <main className="flex h-[80vh] items-center justify-center">
-        <div className="border-destructive text-destructive flex items-center justify-center gap-4 rounded-lg border px-6 py-8 shadow-lg">
-          <CircleAlert size={32} />
+      <Container className="flex-1 items-center justify-center">
+        <Card className="ring-primary shadow-[0px_0px_10px_1px_var(--color-primary)]">
+          <CardHeader>
+            <CardTitle className="text-primary flex flex-col items-center gap-3 text-xl tracking-wider uppercase md:text-2xl">
+              <SIcon icon="FILE_NOT_FOUND" className="text-primary size-7" strokeWidth={2} />
+              Page Not Found
+            </CardTitle>
+          </CardHeader>
 
-          <div className="flex flex-col gap-4">
-            <h1>Access Denied!</h1>
-            <div>
-              The resource you are trying to access is restricted or not
-              available at the moment.
+          <CardContent className="flex flex-col gap-6">
+            <div className="space-y-1">
+              <Typography className="text-base">
+                Sorry, we couldn't find the page you're looking for.
+              </Typography>
+              <Typography className="text-base">
+                It might have been moved or no longer exists.
+              </Typography>
             </div>
-          </div>
-        </div>
-      </main>
+
+            <Button nativeButton={false} render={<Link href="/">Go back</Link>} />
+          </CardContent>
+        </Card>
+      </Container>
     </>
   );
 }
